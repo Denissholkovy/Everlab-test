1bfb6e4068d732a0885647d85908bbbd266a857c
Denis Sholkovy <denissholkovy@gmail.com>
2024-01-24 12:24:30 +0200

    fix

---
+++ javascript/components/gallery.js
@@ -0,0 +1,9 @@
+// gallery
+
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = new ChiefSlider(".slider", {
+      loop: true,
+      autoplay: true,
+      interval: 700000,
+    });
+  });
